b4be6374b0de1a4bd8adfc1f9699cbb095e9cbaf
Jonas Felix <jonas.medeiros.felix@gmail.com>
2022-01-26 09:55:25 -0300

    .

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,6 @@
-import config from 'config';
 import { Pool } from 'pg';
 
-const connectionString = config.get<string>('postgres://pfebmzxh:m0LhBmi3s4tV-VYLuykpZNigUnGv1m3b@kesavan.db.elephantsql.com/pfebmzxh');
+const connectionString = 'postgres://pfebmzxh:m0LhBmi3s4tV-VYLuykpZNigUnGv1m3b@kesavan.db.elephantsql.com/pfebmzxh';
 
 const db = new Pool({ connectionString });
 
