500c5e69b1c81d9390ab9b8b9ebd7bcc8cc87737
Jonas Felix <jonas.medeiros.felix@gmail.com>
2022-01-26 09:54:33 -0300

    correcting database

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
+import config from 'config';
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://pfebmzxh:m0LhBmi3s4tV-VYLuykpZNigUnGv1m3b@kesavan.db.elephantsql.com/pfebmzxh';
+const connectionString = config.get<string>('postgres://pfebmzxh:m0LhBmi3s4tV-VYLuykpZNigUnGv1m3b@kesavan.db.elephantsql.com/pfebmzxh');
 
 const db = new Pool({ connectionString });
 
